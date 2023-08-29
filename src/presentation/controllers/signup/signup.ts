bd552f1665fc6469de73d0fa4358e8e08fa73f83
Wesley Spectre <wesleychavesdev@gmail.com>
2023-08-29 16:04:44 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,6 +1,7 @@
 import { InvalidParamError } from '../../errors/invalid-param-error'
 import { HttpRequest, HttpResponse, badRequest } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
+import { ServerError } from '../../errors/server-error'
 
 export interface EmailValidator {
   isValid (email: string): boolean
@@ -14,21 +15,29 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new Error(`Missing param: ${field}`))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new Error(`Missing param: ${field}`))
+        }
       }
-    }
 
-    const { email, password, passwordConfirmation } = httpRequest.body
-    if (password !== passwordConfirmation) {
-      return badRequest(new InvalidParamError('passwordConfirmation'))
-    }
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
 
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
